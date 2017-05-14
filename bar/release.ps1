5856df7cfc712ececbbdb85e15bd1d14e1833ad6
Steven Rose <steven@bigdataforhumans.com>
2017-05-14 14:54:01 +0100

    Updated version number

--- bar/release.ps1
+++ bar/release.ps1
@@ -2,6 +2,6 @@ Write-Output 'releasing ...'
 
 $ApiKey = $Env:ApiKey
 
-Publish-Module  -Name SplattyPS -NuGetApiKey $ApiKey
+Publish-Module  -Name SplattyPS -NuGetApiKey $ApiKey -RequiredVersion 0.2
 
 Write-Output 'releasing - done.'
