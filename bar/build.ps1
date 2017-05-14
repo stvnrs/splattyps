9eb62176f237e1ecb7daba35c7257d62812be7f7
Steven Rose <steven@bigdataforhumans.com>
2017-05-14 14:09:01 +0100

    updated build script

--- bar/build.ps1
+++ bar/build.ps1
@@ -1,6 +1,16 @@
+Set-StrictMode -Version 'Latest'
+$ErrorActionPrefernce = 'Stop'
+
 Write-Output 'building ...'
 
+if(Test-Path .\out){
+    rm .\out\* -Recurse
+} else {
+    md .\out
+}
 Copy-Item .\src\SplattyPS.* .\out\
+
+Import-Module PlatyPS
 New-ExternalHelp -Path .\docs\ -OutputPath .\out\en-us\ -Force
 
 Write-Output 'building - done.'
